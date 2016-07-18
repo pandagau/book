7058ad8a765a71d270435e99885832d10c2a265b
pandagau <thuhien003@gmail.com>
2016-07-18 16:04:37 +0700

    hien commit

--- view/viewbook.php
+++ view/viewbook.php
@@ -5,6 +5,7 @@
   /* view book commit*/
     <?php   
   		//echo "abcd".isset($_GET['book']);
+  		//hien
         echo 'Title:' . $book->title . '<br/>';  
         echo 'Author:' . $book->author . '<br/>';  
         echo 'Description:' . $book->description . '<br/>';  

8380c08e4ef8e480f7f36e09c722efb3ea163b06
pandagau <thuhien003@gmail.com>
2016-07-18 16:09:03 +0700

    hien 2

--- view/viewbook.php
+++ view/viewbook.php
@@ -6,6 +6,7 @@
     <?php   
   		//echo "abcd".isset($_GET['book']);
   		//hien
+  		//hien 2
         echo 'Title:' . $book->title . '<br/>';  
         echo 'Author:' . $book->author . '<br/>';  
         echo 'Description:' . $book->description . '<br/>';  

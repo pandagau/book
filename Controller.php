b98c2facd8c3f65b305a73c5c7ac3325a51f7e35
pandagau <thuhien003@gmail.com>
2016-07-18 15:06:39 +0700

    commit lan 2
    line 4

---
+++ Controller.php
@@ -0,0 +1,29 @@
+<?php 
+//add comment commit
+	include_once("model/Model.php");  
+  //edit lan 2 hien
+	class Controller {  
+	     public $model;   
+	  
+	     public function __construct()    
+	     {    
+	          $this->model = new Model();  
+	     }   
+	     public function invoke()  
+	     {  
+	          if (!isset($_GET['user']))  
+	          {  
+	               // no special book is requested, we'll show a list of all available books  
+	               $books = $this->model->getBookList();  
+	               include 'view/booklist.php'; 
+	          } 
+	          else 
+	          { 	echo $_GET['user'];
+	               // show the requested book 
+	               $book = $this->model->getBook($_GET['user']); 
+	               include 'view/viewbook.php';  
+	          }  
+	          //add comment
+	     }  
+	}  
+ ?>
